c908859999f4dad05a2dd30a69d345289b474162
djbchi-young <99797656+djbchi-young@users.noreply.github.com>
2022-09-10 13:02:08 +0800

    Update Ex2_Truth_ChiYoung.java
    Made sure every single input the user makes is valid

--- 1stQuarter/Ex2_Truth_ChiYoung.java
+++ 1stQuarter/Ex2_Truth_ChiYoung.java
@@ -21,32 +21,83 @@ public class Ex2_Truth_ChiYoung {
     
     static void changeSettings(){
         Scanner sc = new Scanner(System.in);
+        //boolean for checking if inputs are valid
         boolean isValid = true;
         do{
+        
         System.out.print("What is the lower limit of the random number? ");
-        lowLimit = Integer.parseInt(sc.nextLine());
-        System.out.print("What is the upper limit of the random number ? ");
-        highLimit = Integer.parseInt(sc.nextLine());
-        if(lowLimit>=highLimit){
-            isValid = false;
-            System.out.println("Please input an upper limit that is greater than the lower limit.");
+        //creates a string variable to be parsed into an integer later
+        String lowLimitInput = sc.nextLine();
+        //checks if the input isn't empty
+        if(lowLimitInput.length() > 0) {
+            //checks if the input is an integer
+            try {
+                lowLimit = Integer.parseInt(lowLimitInput);
+                isValid = true;}catch(NumberFormatException e) {
+                isValid = false;
+                System.out.println("Please input an integer.");}
+            }else isValid = false;
+        //runs if lower limit is valid
+        if(isValid){
+            System.out.print("What is the upper limit of the random number ? ");
+            String highLimitInput = sc.nextLine();
+        if(lowLimitInput.length() > 0) {
+            try {
+                highLimit = Integer.parseInt(highLimitInput);
+                isValid = true;}catch(NumberFormatException e) {
+                isValid = false;
+                System.out.println("Please input an integer.");}
+            }else isValid = false;
+        //checks if lower limit is greater than upperlimit
+            if(lowLimit>=highLimit){
+                isValid = false;
+                System.out.println("Please input integers and input an upper limit that is greater than the lower limit.");
+            }
         }
-        else{System.out.print("How many guesses are allowed? ");
-        guessCount = Integer.parseInt(sc.nextLine());
+        //runs if 2 previous answers were valid
+        if (isValid){System.out.print("How many guesses are allowed? ");
+        String guessCountInput = sc.nextLine();
+        if(lowLimitInput.length() > 0) {
+            try {
+                guessCount = Integer.parseInt(guessCountInput);
+                //number of guesses should only be positive
+                if(guessCount<=0){
+                isValid = false;
+                System.out.println("Please input a positive integer.");}
+                else isValid = true;}catch(NumberFormatException e) {
+                isValid = false;
+                System.out.println("Please input a positive integer.");}
+            }else isValid = false;
         //remove when finished testing!!
         //System.out.println(lowLimit);
         //System.out.println(highLimit);
         //System.out.println(guessCount);
-        isValid = true;}
-        }while(!isValid);
+        }
+        }while(!isValid); //does it again when the 
     }
     static void playAgain(){
         Scanner sc = new Scanner(System.in);
         boolean isYesNo = true;
+        boolean isYesNoTwo = true;
         do{ System.out.print("Play again (y/n)? ");
             String yesNo = sc.nextLine();
             switch(yesNo){
                 case "y":
+                    do{ System.out.print("Would you like to change the settings (y/n)? ");
+                        String yesNoTwo = sc.nextLine();
+                        switch(yesNoTwo){
+                            case "y":
+                                changeSettings();
+                                break;
+                            case "n":
+                                startGame();
+                            break;
+                            default:
+                                System.out.println("Please input either y or n");
+                                isYesNoTwo = false;
+
+                        }
+        }while(!isYesNoTwo);
                     startGame();
                     break;
                 case "n":
